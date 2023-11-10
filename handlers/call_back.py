ffa29dc9241728a7f976f1666731da68d87b1511
akhletdin <ahletdinmacansin@gmail.com>
2023-11-10 15:09:56 +0600

    hw-1

---
+++ handlers/call_back.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+from aiogram import types, Dispatcher
+from config import bot
+from database.sql_commands import Database
+from keyboards.inline_buttons import questionnaire_keyboard
+
+
+async def start_questionnaire_call(call: types.CallbackQuery):
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="Python or Mojo ?",
+        reply_markup=await questionnaire_keyboard()
+    )
+
+
+async def python_call(call: types.CallbackQuery):
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="U R Python Developer 🐍"
+    )
+
+
+async def mojo_call(call: types.CallbackQuery):
+    await bot.send_message(
+        chat_id=call.from_user.id,
+        text="U R Mojo Developer 🔥"
+    )
+
+
+def register_callback_handlers(dp: Dispatcher):
+    dp.register_callback_query_handler(start_questionnaire_call,
+                                       lambda call: call.data == "start_questionnaire")
+    dp.register_callback_query_handler(python_call,
+                                       lambda call: call.data == "python")
+    dp.register_callback_query_handler(mojo_call,
+                                       lambda call: call.data == "mojo")
---
+++ main.py
@@ -0,0 +1,23 @@
+from aiogram import executor
+from config import dp
+from handlers import (
+    start,
+    call_back
+)
+from database import sql_commands
+
+
+async def on_startup(_):
+    db = sql_commands.Database()
+    db.sql_create_tables()
+
+
+start.register_start_handlers(dp=dp)
+call_back.register_callback_handlers(dp=dp)
+
+if __name__ == "__main__":
+    executor.start_polling(
+        dp,
+        skip_updates=True,
+        on_startup=on_startup
+    )
